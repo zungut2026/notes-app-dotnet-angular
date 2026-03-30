b79012ecff9c50a2a947a6bfab64d7019cec5ddb
Thandeka Zungu <ZunguT@smd.co.za>
2026-03-30 16:07:40 +0200

    Initial commit

---
+++ Controller/NoteController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/notes")]
+public class NotesController : ControllerBase
+{
+    //private readonly NoteService _service;
+
+    //public NotesController(NoteService service)
+    //{
+    //    _service = service;
+    //}
+
+    [HttpGet]
+    public IActionResult Test()
+    {
+        return Ok("API is working");
+    }
+
+    //[HttpPost("get-notes")]
+    //public async Task<IActionResult> GetNotes([FromBody] GetNotesRequest request)
+    //{
+    //    var result = await _service.GetNotes(request);
+    //    return Ok(result);
+    //}
+
+    //[HttpPost]
+    //public async Task<IActionResult> Create([FromBody] NoteDto dto)
+    //{
+    //    var result = await _service.CreateNote(dto);
+    //    return Ok(result);
+    //}
+}
+    
---
+++ Models/Tag.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+using System;
+
+public class Tag
+{
+    public int TagId { get; set; }
+    public string TagName { get; set; }
+}
